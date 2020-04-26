a201851a2b83b702c74a6cb7da76d9b21d6c7786
OLUWASEYI OLA <olaoluwaseyi81@gmail.com>
2020-04-26 14:13:45 -0500

    Delete OnShipmentListener.java
    Interface not needed at we are not in need of any action when a shipment card is clicked

--- app/src/main/java/com/generic/androidtracker/interfaces/OnShipmentListener.java
+++
@@ -1,9 +0,0 @@
-package com.generic.androidtracker.interfaces;
-
-public interface OnShipmentListener {
-    /**
-     * Handles the action event when a shipment item view holder is clicked on.
-     * @param position
-     */
-    void onShipmentClicked(int position);
-}
